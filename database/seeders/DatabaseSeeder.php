9377725d3319ff760c65273a28ed5dfa2b7a7469
KJordan <shadow902@gmail.com>
2024-08-25 19:48:48 -0400

    create base users and assign Admin role

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +16,20 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $user = User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
         ]);
+        $role = Role::create(['name' => 'Admin']);
+        $user->assignRole($role);
+        User::factory()->create([
+            'name' => 'Financial',
+            'email' => 'financial@gmail.com',
+        ]);
+        User::factory()->create([
+            'name' => 'Officer',
+            'email' => 'officer@gmail.com',
+        ]);
+
     }
 }
